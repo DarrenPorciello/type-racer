a9befd7a0f5109defe808ec0e19b62bc08fc6877
Darren Porciello <darren.porciello1@gmail.com>
2025-03-17 12:37:16 -0400

    Working build

---
+++ config/keys_dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+    mongoURI: "mongodb://localhost:27017/rocket-typer",
+    secretOrKey: "my_secret_key"
+  };
+  // MUST CHANGE THIS LATER
+  // Not a real mongo db instance. Simply created to compile.
